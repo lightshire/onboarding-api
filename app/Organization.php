6901427bcef2e242792aefb871c6359bcb85de1f
Gian Santillan <giansantillan18@gmail.com>
2019-09-30 13:01:08 +0800

    fixed ide helper

--- app/Organization.php
+++ app/Organization.php
@@ -4,6 +4,26 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Organization
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string $code
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Organization newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Organization newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Organization query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Organization whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Organization whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Organization whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Organization whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Organization whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Organization whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Organization extends Model
 {
     protected $fillable = [
