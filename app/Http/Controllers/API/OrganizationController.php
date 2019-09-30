74ed5942399fe33e21823b66bb7d4264bcc02d55
Gian Santillan <giansantillan18@gmail.com>
2019-09-30 14:15:20 +0800

    added organization

--- app/Http/Controllers/API/OrganizationController.php
+++ app/Http/Controllers/API/OrganizationController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Organization;
 
+/**
+ * @group Organization API
+ * Class OrganizationController
+ * @package App\Http\Controllers\API
+ */
 class OrganizationController extends Controller
 {
-
+    /**
+     * Get Organization information
+     * @transformer \App\Transformers\OrganizationTransformer
+     * @transformerModel \App\Organization
+     *
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getOrganization($id)
+    {
+        return responder()->success(Organization::find($id))->respond();
+    }
 }
---
+++ app/Transformers/OrganizationTransformer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+
+class OrganizationTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform()
+    {
+        return [
+            //
+        ];
+    }
+}
